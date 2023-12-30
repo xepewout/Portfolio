e1187235625a1373b44259e08233ec002da24dc0
jorryns <jorryn.salvador@gmail.com>
2023-12-30 03:30:33 -0800

    test

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/images">Images</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/images" element={<ImagesPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      {/* Additional content */}
+    </div>
+  );
+}
+
+function ImagesPage() {
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap', background: 'white' }}>
+      {Array.from({ length: 100 }).map((_, index) => (
+        <img 
+          key={index} 
+          src={`https://nixon-s610.s3.us-west-1.amazonaws.com/DSCN` + String(index + 1).padStart(4, '0') + `.JPG`} 
+          alt={`DOES NOT EXIST ${index}`} 
+          style={{ margin: '5px', width: '630px'}} 
+        />
+      ))}
+    </div>
+  );
+}
+
+
+export default App;
