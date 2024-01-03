2b5761faf0a505bf930badeb1b9c9ed65eb04a8f
jorryns <jorryn.salvador@gmail.com>
2024-01-03 13:37:11 -0800

    update 3
    -added new icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+
+function MediaItem({ baseName }) {
+  const [hasImageError, setHasImageError] = useState(false);
+
+  const handleImageError = () => {
+    setHasImageError(true);
+  };
+
+  return (
+    <>
+      {!hasImageError ? (
+        <img 
+          src={`${baseName}.JPG`} 
+          onError={handleImageError} 
+          alt="" 
+          style={{ margin: '5px', width: '620px'}}
+        />
+      ) : (
+        <video 
+          style={{ margin: '5px', width: '620px'}} 
+          controls
+        >
+          <source src={`${baseName}.mp4`} type="video/mp4" />
+          Your browser does not support this video format.
+        </video>
+      )}
+    </>
+  );
+}
 
 function App() {
   return (
@@ -28,22 +58,66 @@ function HomePage() {
 }
 
 function ImagesPage() {
+  const totalImages = 160;
+  const imagesPerPage = 50;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastImage = totalImages - (currentPage - 1) * imagesPerPage;
+  const indexOfFirstImage = Math.max(indexOfLastImage - imagesPerPage, 0);
+  const currentImages = Array.from({ length: totalImages }).slice(indexOfFirstImage, indexOfLastImage).reverse();
+
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', background: 'white' }}>
-      {Array.from({ length: 109 }).map((_, index, arr) => {
-        const reverseIndex = arr.length - 1 - index;
-        return (
-          <img 
-            key={reverseIndex} 
-            src={`https://nixon-s610.s3.us-west-1.amazonaws.com/DSCN` + String(reverseIndex + 1).padStart(4, '0') + `.JPG`} 
-            alt={`DOES NOT EXIST ${reverseIndex}`} 
-            style={{ margin: '5px', width: '620px'}} 
-          />
-        );
-      })}
+    <div>
+      <Pagination totalImages={totalImages} imagesPerPage={imagesPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <div style={{ display: 'flex', flexWrap: 'wrap', background: 'white' }}>
+        {currentImages.map((_, index) => {
+          const imageIndex = indexOfLastImage - index;
+          const baseName = `https://nixon-s610.s3.us-west-1.amazonaws.com/DSCN` + String(imageIndex).padStart(4, '0');
+          return <MediaItem key={imageIndex} baseName={baseName} />;
+        })}
+      </div>
+      <Pagination totalImages={totalImages} imagesPerPage={imagesPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
+
+
+function Pagination({ totalImages, imagesPerPage, currentPage, setCurrentPage }) {
+  const totalPages = Math.ceil(totalImages / imagesPerPage);
+
+  const getPageTitle = pageNumber => {
+    const startNumber = totalImages - (pageNumber - 1) * imagesPerPage;
+    const endNumber = Math.max(startNumber - imagesPerPage + 1, 1);
+    return `${startNumber} - ${endNumber}`;
+  };
+
+  const handleClick = (e, pageNumber) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <nav>
+      <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'center' }}>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map(pageNumber => (
+          <li key={pageNumber} style={{ margin: '10px' }}>
+            <a 
+              href="#" 
+              onClick={(e) => handleClick(e, pageNumber)} 
+              style={{ textDecoration: currentPage === pageNumber ? 'underline' : 'none' }}
+            >
+              {getPageTitle(pageNumber)}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+
+
+
 
 
 
